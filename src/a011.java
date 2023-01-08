70f026b5c643a62166e0e3024d653cd30cd9dc85
senturk2020 <ahmetsenturk20@gmail.com>
2023-01-08 21:18:34 +0300

    ortakm

--- src/a011.java
+++ src/a011.java
@@ -2,6 +2,7 @@ public class a011 {
     public static void main(String[] args) {
 
         System.out.println("hello java");
+        System.out.println("bo ortak class");
     }
 
 }
--- src/deneme_ahmet.java
+++ src/deneme_ahmet.java
@@ -2,5 +2,7 @@ public class deneme_ahmet {
 
     public static void main(String[] args) {
         System.out.println("deneme ahmet deneme ");
+
+
     }
 }
