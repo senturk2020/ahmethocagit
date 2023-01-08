d5cb6d8760ee4671fe81104617b0fad0edbdb24f
senturk2020 <ahmetsenturk20@gmail.com>
2023-01-08 21:14:21 +0300

    2.denem

--- src/deneme_ahmet.java
+++ src/deneme_ahmet.java
@@ -2,5 +2,9 @@ public class deneme_ahmet {
 
     public static void main(String[] args) {
         System.out.println("deneme ahmet deneme ");
+
+        System.out.println( " 2 deneme ");
     }
+
+
 }
